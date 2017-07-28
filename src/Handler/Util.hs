98f3a902458ff9c6229b3841c20e4eec5fc01806
Vladimir Pankov <wowofbob@gmail.com>
2017-07-28 15:48:01 -0400

    Added runner for handler in ExceptT.

--- src/Handler/Util.hs
+++ src/Handler/Util.hs
@@ -5,11 +5,36 @@
 {-# LANGUAGE TypeFamilies        #-}
 module Handler.Util where
 
-import ClassyPrelude.Yesod hiding (Proxy)
+import ClassyPrelude.Yesod hiding (Proxy, Text, pack)
 import Control.Monad.Except
 import Data.Aeson.Types
+import Data.Text (Text, pack)
 import Data.Typeable
 import Yesod.Auth
+
+
+-- * Runner for ExceptT handler.
+
+newtype Answer a = Answer { toEither :: Either Text a }
+
+instance ToJSON a => ToJSON (Answer a) where
+  toJSON (Answer (Left msg)) = object
+    [ "error"   .= True
+    , "message" .= msg
+    , "data"    .= Null
+    ]
+  toJSON (Answer (Right val)) = object
+    [ "error"   .= False
+    , "message" .= pack "OK"
+    , "data"    .= val
+    ]
+
+answerToRequest
+  :: ToJSON a
+  => ExceptT Text (HandlerT site IO) a
+  -> HandlerT site IO Value
+answerToRequest request =
+  runExceptT request >>= returnJson . Answer 
 
 
 -- * runDB lifted to ExceptT
