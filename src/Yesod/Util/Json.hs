aab695aeee7336c860fa7928b187a48e7095404e
Vladimir Pankov <vladap@fiord.com>
2018-01-11 10:23:43 +0300

    Moved generic functions to the bottom; askValue doesn't depened from JsobObjEnv

--- src/Yesod/Util/Json.hs
+++ src/Yesod/Util/Json.hs
@@ -103,23 +103,6 @@ instance (MonadHandler m) => MonadHandler (JsonObjEnv m) where
   type HandlerSite (JsonObjEnv m) = HandlerSite m
   liftHandlerT = lift . liftHandlerT
 
-
--- | Parse request body to json 'Value'.
-parseJsonValue :: (MonadError Text m, MonadHandler m) => m Value
-parseJsonValue = do
-  eValue <- rawRequestBody $$ runCatchC (sinkParser value')
-  case eValue of
-    Left  e -> throwError $ pack $ show e
-    Right v -> pure v
-
--- | Parse request body to json 'Value'; throw error on every case except 'Object'.
-parseJsonObject :: (MonadError Text m, MonadHandler m) => m Object
-parseJsonObject = do
-  v <- parseJsonValue
-  case v of
-    Object obj -> pure obj
-    _          -> throwError "json object expected"
-
 -- | Evaluate 'JsonObjEnv' to 'ExceptJ'.
 runJsonObjEnv :: (MonadHandler m) => JsonObjEnv m a -> ExceptJ m a
 runJsonObjEnv f =
@@ -129,24 +112,6 @@ runJsonObjEnv f =
 withJsonObjEnv :: (MonadHandler m, ToJSON a) => JsonObjEnv m a -> m Value
 withJsonObjEnv = runExceptJ . runJsonObjEnv
 
-
--- | Ask json object value by key.
-askValue
-  :: forall m a .
-      (Monad m, FromJSON a, Typeable a) => Text -> JsonObjEnv m a
-askValue key = do 
-  obj <- ask
-  case parse_ (obj .: key >>= parseJSON) of
-    Success a -> pure a
-    Error   _ -> throwError (errMsg key)
-  where
-    parse_ p = parse (const p) ()
-    errMsg k = pack $
-      "cannot parse '"
-        ++ show (typeRep (Proxy :: Proxy a))
-          ++ "' associated with key '"
-            ++ show k
-              ++ "'"
 
 -- | Get 'Entity' from database using 'Key' from json object.
 askEntity
@@ -170,7 +135,23 @@ askEntityWhich p key = do
   askValue key >>= getEntityWhich p
   
 
--- | Wrapper for 'parseJsonBody'.
+-- | Parse request body to json 'Value'.
+parseJsonValue :: (MonadError Text m, MonadHandler m) => m Value
+parseJsonValue = do
+  eValue <- rawRequestBody $$ runCatchC (sinkParser value')
+  case eValue of
+    Left  e -> throwError $ pack $ show e
+    Right v -> pure v
+
+-- | Parse request body to json 'Value'; throw error on every case except 'Object'.
+parseJsonObject :: (MonadError Text m, MonadHandler m) => m Object
+parseJsonObject = do
+  v <- parseJsonValue
+  case v of
+    Object obj -> pure obj
+    _          -> throwError "json object expected"
+
+-- | Lift 'parseJsonBody' to 'MonadError'.
 getJson
   :: forall m a .
        (MonadError Text m, MonadHandler m, FromJSON a, Typeable a) => m a
@@ -181,3 +162,25 @@ getJson = do
     Error   _    -> throwError . pack $
                       "invalid json for " ++
                          show (typeRep (Proxy :: Proxy a))
+
+-- | Retrieve json object field by key.
+askValue
+  :: forall m a .
+       ( MonadError Text m
+       , MonadReader Object m
+       , FromJSON a
+       , Typeable a
+       ) => Text -> m a
+askValue key = do
+  obj <- ask
+  case parse_ (obj .: key >>= parseJSON) of
+    Success a -> pure a
+    Error   _ -> throwError (errMsg key)
+  where
+    parse_ p = parse (const p) ()
+    errMsg k = pack $
+      "cannot parse '"
+        ++ show (typeRep (Proxy :: Proxy a))
+          ++ "' associated with key '"
+            ++ show k
+              ++ "'"
