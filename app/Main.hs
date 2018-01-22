3e41db3ec78abf74d83a5ea52e51cb357593b60c
Vladimir Pankov <vladap@fiord.com>
2018-01-22 23:02:44 +0300

    Added minimal yesod site to Main.hs (see Yesod Book, Basics)

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 module Main where
 
+import Yesod
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod App
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
 main :: IO ()
-main = pure ()
+main = warp 3000 App
